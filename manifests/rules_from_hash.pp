ac404eba64e6f5ec7d8764d32e6c27689079b684
Ben Agricola <bagricola@squiz.co.uk>
2013-05-08 23:29:54 +0100

    Removed hiera lookups on access, activemq, iptables, mcollective, motd, nagios, nrpe, ntp, openssh, postfix.
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- manifests/rules_from_hash.pp
+++ manifests/rules_from_hash.pp
@@ -4,9 +4,11 @@
 # iptables rules from a hash within hiera. Depending on
 # hiera merge settings, this can merge rules from multiple
 # levels.
-class iptables::rules_from_hash inherits iptables::params {
+class iptables::rules_from_hash (
+  $rules          = params_lookup( 'rules', '', 'hash' ),
+  $rule_defaults  = params_lookup( 'rule_defaults', '', 'hash' ),
+) inherits iptables::params {
   include iptables
 
-  $rules = hiera_hash("iptables::rules",$default_rules)
   create_resources('iptables::rule',$rules,$rule_defaults)
 }
